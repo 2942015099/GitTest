d6cf3cc98df4b25bf0198b2adb956321716b1261
2942015099 <2942015099@qq.com>
2019-08-20 10:33:58 +0800

    我是 xxx

--- app/src/main/java/com/example/administrator/gittest/MainActivity.java
+++ app/src/main/java/com/example/administrator/gittest/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//123456   ren hongyu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
